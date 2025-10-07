36d84a582c850e0fe1d3419aa80e7c62cdd46016
Laura-ISC <laujocelyn31@gmail.com>
2025-10-06 23:38:20 -0600

    Add files via upload

---
+++ EVA2_9_FOR/src/eva2_9_for/EVA2_9_FOR.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_9_for;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author laura
+ */
+public class EVA2_9_FOR {
+
+    public static void main(String[] args) {
+       Scanner captu = new Scanner(System.in);
+       int rep;
+       String mensaje;
+        System.out.println("Mensaje a repetir: ");
+        mensaje = captu.nextLine();
+        System.out.println("Cantidad de repeticiones");
+        rep = captu.nextInt();
+        for(int i = 1; i <= rep; i++){
+            System.out.println(mensaje);   
+        }
+     
+    }
+    
+}
