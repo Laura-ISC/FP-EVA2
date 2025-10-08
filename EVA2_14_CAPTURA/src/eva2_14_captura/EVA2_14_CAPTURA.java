f6ad7def77c066e393722d8b96674c864200f21c
Laura-ISC <laujocelyn31@gmail.com>
2025-10-08 16:49:50 -0600

    Add files via upload

---
+++ EVA2_14_CAPTURA/src/eva2_14_captura/EVA2_14_CAPTURA.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_14_captura;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author laura
+ */
+public class EVA2_14_CAPTURA {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner(System.in);
+        int tama;
+        System.out.println("¿Cuantas calificaciones va a capturar?");
+        tama = captu.nextInt();
+        int califas[] = new int [tama];
+        //CAPTURAR
+        for(int i = 0; i < califas.length; i++){
+            System.out.println("Calificación " + (i + 1) + ":");
+            califas[i] = captu.nextInt();
+        }
+        //LEER
+        for(int i = 0; i < califas.length; i++){
+            System.out.print("[" + califas[i] + "]");
+        }    
+    }
+    
+}
