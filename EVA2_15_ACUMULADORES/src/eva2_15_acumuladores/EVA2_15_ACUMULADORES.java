6a5fef4e690131eab922ff49f301dcc3d75398b9
Laura-ISC <laujocelyn31@gmail.com>
2025-10-15 16:52:52 -0600

    Add files via upload

---
+++ EVA2_15_ACUMULADORES/src/eva2_15_acumuladores/EVA2_15_ACUMULADORES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_acumuladores;
+
+/**
+ *
+ * @author laura
+ */
+public class EVA2_15_ACUMULADORES {
+
+
+    public static void main(String[] args) {
+       //ACUMULADOR:
+       //++  --  +=  -= *= /=
+       int cuenta = 0;
+       // ASIGNACIÓN --> =
+        System.out.println(cuenta);
+       cuenta = cuenta + 100; //acumulador
+        System.out.println(cuenta);
+       cuenta = cuenta + 1000;
+        System.out.println(cuenta);
+       cuenta = cuenta - 800; //cuenta-=
+        System.out.println(cuenta);
+        cuenta += 2000; //cuenta = cuenta + 2000;
+        System.out.println(cuenta);
+    }
+    
+}
