8a312fa118471cfe6b2abefbd795a2db3ce409b3
Laura-ISC <laujocelyn31@gmail.com>
2025-10-28 16:53:22 -0600

    Add files via upload

---
+++ EVA2_18_WHILE/src/eva2_18_while/EVA2_18_WHILE.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_while;
+
+/**
+ *
+ * @author laura
+ */
+public class EVA2_18_WHILE {
+
+    public static void main(String[] args) {
+        System.out.println("CICLO WHILE--------");
+        int i = 1;
+        while(i<=10 ){//se repiiten si la condición es verdad
+            System.out.println("Laura Granados");
+            i++;
+        }
+        //SIMILAR UN FOR CON DO WHILE
+        System.out.println("CICLO DO WHILE--------");
+        i = 1;
+        do{
+            System.out.println("Laura Granados"); 
+            i++;
+        }while(i<=10);
+        
+    }
+    
+}
